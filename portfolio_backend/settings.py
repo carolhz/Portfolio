b65db452227eb781e15e2702299f11084d85b66d
carolhz <150878380+carolhz@users.noreply.github.com>
2025-10-23 02:32:47 +0700

    Update CORS_ALLOWED_ORIGINS for frontend access

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -146,8 +146,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://localhost:3000",
+    "http://localhost:5173",  
+    "https://portfolio-frontend-blush-seven.vercel.app", 
 ]
 
 MEDIA_URL = '/media/'
