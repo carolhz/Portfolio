c85451249e83f4cdbcff8c8ef154c13f0cbc96ca
carolhz <150878380+carolhz@users.noreply.github.com>
2025-10-23 09:04:24 +0700

    Remove JWT token routes from urls.py
    Removed JWT authentication URLs from urlpatterns.

--- portfolio_backend/urls.py
+++ portfolio_backend/urls.py
@@ -4,19 +4,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-# Import ini
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    
-    # URL UNTUK LOGIN API
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/', include('api.urls')), 
 ]
 
 if settings.DEBUG:
