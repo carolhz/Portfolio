73dee3adeb23795b656a24e787c0dd0188ba5ba7
carolhz <150878380+carolhz@users.noreply.github.com>
2025-10-23 09:03:51 +0700

    Add JWT token views to API URLs

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = DefaultRouter()
 router.register(r'profile', views.ProfileViewSet)
@@ -12,4 +16,6 @@ router.register(r'projects', views.ProjectViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('contact/', views.ContactFormView.as_view(), name='contact-form'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
