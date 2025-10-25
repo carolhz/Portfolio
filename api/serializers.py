76ce7a09878995320509b395c371e078c9737556
carolhz <150878380+carolhz@users.noreply.github.com>
2025-10-25 16:40:34 +0700

    Enhance serializers with image URL methods
    Added serializer methods to include image URLs for ProjectImage and Profile.

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,3 @@
-# api/serializers.py
 from rest_framework import serializers
 from .models import Profile, Skill, Tool, Project, ProjectImage
 
@@ -13,9 +12,19 @@ class ToolSerializer(serializers.ModelSerializer):
         fields = ['id', 'nama']
 
 class ProjectImageSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+    
     class Meta:
         model = ProjectImage
         fields = ['id', 'image']
+    
+    def get_image(self, obj):
+        if obj.image:
+            request = self.context.get('request')
+            if request:
+                return request.build_absolute_uri(obj.image.url)
+            return obj.image.url
+        return None
 
 class ProjectSerializer(serializers.ModelSerializer):
     skills = serializers.PrimaryKeyRelatedField(
@@ -26,7 +35,8 @@ class ProjectSerializer(serializers.ModelSerializer):
     )
     
     images = ProjectImageSerializer(many=True, read_only=True)
-
+    gambar_thumbnail = serializers.SerializerMethodField()
+    
     class Meta:
         model = Project
         fields = [
@@ -34,22 +44,43 @@ class ProjectSerializer(serializers.ModelSerializer):
             'link_demo', 'link_repo', 'skills', 'tools',
             'role', 'images'
         ]
-
+    
+    def get_gambar_thumbnail(self, obj):
+        if obj.gambar_thumbnail:
+            request = self.context.get('request')
+            if request:
+                return request.build_absolute_uri(obj.gambar_thumbnail.url)
+            return obj.gambar_thumbnail.url
+        return None
+    
     def to_representation(self, instance):
         representation = super().to_representation(instance)
         representation['skills'] = SkillSerializer(instance.skills, many=True).data
         representation['tools'] = ToolSerializer(instance.tools, many=True).data
         
-        # --- INI ADALAH PERBAIKANNYA ---
-        # Tambahkan baris ini untuk menyertakan gambar galeri
-        representation['images'] = ProjectImageSerializer(instance.images, many=True).data
+        # Pass context ke ProjectImageSerializer
+        representation['images'] = ProjectImageSerializer(
+            instance.images, 
+            many=True, 
+            context=self.context
+        ).data
         
         return representation
 
 class ProfileSerializer(serializers.ModelSerializer):
+    foto = serializers.SerializerMethodField()
+    
     class Meta:
         model = Profile
         fields = [
             'id', 'nama', 'deskripsi_singkat', 'deskripsi_lengkap',
             'foto', 'visi', 'misi', 'linkedin', 'github', 'email'
         ]
+    
+    def get_foto(self, obj):
+        if obj.foto:
+            request = self.context.get('request')
+            if request:
+                return request.build_absolute_uri(obj.foto.url)
+            return obj.foto.url
+        return None
